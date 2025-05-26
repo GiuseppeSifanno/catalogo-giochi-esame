e5fd8765fb15f3fda0811ad92def2255c9e22ecf
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2025-05-26 20:30:36 +0200

    Modifiche per effettuare prove
    Signed-off-by: GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>

--- catalogo.c
+++ catalogo.c
@@ -5,19 +5,26 @@
 int main(void) {
 
     char query[MAX_CHAR] = "#fps";
-
+/*
     for (int i = 0; i < 3; i++) {
         aggiungiGioco(inserisciGioco());
     }
-
-    unsigned short num_elementi = 0;
+*/
+    unsigned short num_elementi;
     long* offset = ricercaGlobale(query, &num_elementi);
-    gioco_t gioco;
+
     for (int i = 0; i < num_elementi; i++) {
         printf("%lo\n", offset[i]);
-        ricercaSpecifica(offset[i], &gioco);
+        gioco_t gioco = ricercaSpecifica(offset[i]);
         printf("%s\n", gioco.titolo);
         printf("%s\n", gioco.generi[0]);
     }
+
+    gioco_t *gioco = ordinaStatistiche(1);
+    int len = sizeof(*gioco)/sizeof(gioco[0]);
+    printf("%d\n", len);
+    printf("%u",gioco[0].copie_vendute);
+    printf("\n%u",gioco[1].copie_vendute);
+
     return 0;
 }
